ff0989144ac5e691ab98becbf3948a9a36605f2c
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-04 13:59:36 +0200

    Ændret udregningsmetoderne til at returnerer en double. Konstruktor indeholder to lokale variabler.

--- SharedCalculatorLibrary/BasicOperations.cs
+++ SharedCalculatorLibrary/BasicOperations.cs
@@ -8,27 +8,54 @@ namespace SharedCalculatorLibrary
 {
     public class BasicOperations
     {
-        public double tal1 { get; set; }
-        public double tal2 { get; set; }
 
-        private void PlusFunktion()
+        /// <summary>
+        /// 2 properties tal 1 og tal 2 af datatypen double så decimaler er muligt.
+        /// </summary>
+        public double Tal1 { get; set; }
+        public double Tal2 { get; set; }
+
+        /// <summary>
+        /// Metode som er void hvori tal1 og tal 2 props plusses med hinanden. 
+        /// </summary>
+
+        public double PlusFunktion()
         {
-            (tal1 + tal2).ToString();           
+            return (Tal1 + Tal2);          
         }
 
-        private void MinusFunktion()
-        {         
-            (tal1 - tal2).ToString();
+        /// <summary>
+        /// Metode som er void hvori tal1 og tal 2 props minus hinanden. 
+        /// </summary>
+
+        public double MinusFunktion()
+        {
+            return (Tal1 - Tal2);
         }
 
-        private void GangeFunktion()
+        /// <summary>
+        /// Metode som er void hvori tal1 og tal 2 props ganges med hinanden. 
+        /// </summary>
+
+        public double GangeFunktion()
         {       
-            (tal1 * tal2).ToString();         
+           return (Tal1 * Tal2);         
         }
 
-        private void DividerFunktion()
-        {       
-           (tal1 / tal2).ToString();         
+        /// <summary>
+        /// Metode som er void hvori tal1 og tal 2 props divideret med hinanden. 
+        /// </summary>
+
+        public double DividerFunktion()
+        {
+            return (Tal1 / Tal2);         
+        }
+
+
+        public BasicOperations(double tal1, double tal2)
+        {
+            this.Tal1 = tal1;
+            this.Tal2 = tal2;
         }
     }
 }
---
+++ SharedCalculatorLibraryTests/BasicOperationsTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculatorLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculatorLibrary.Tests
+{
+    [TestClass()]
+    public class BasicOperationsTests
+    {
+        [TestMethod()]
+        public void PlusFunktionTest4()
+        {
+            //Arrange
+
+            var cal = new BasicOperations(2, 2);
+
+            //Act
+
+            double result = cal.PlusFunktion();
+
+            //Assert
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        public void MinusFunktionTest0()
+        {
+            //Arrange
+
+            var cal = new BasicOperations(2, 2);
+
+            //Act
+
+            double result = cal.MinusFunktion();
+
+            //Assert
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void GangeFunktionTest10()
+        {
+            //Arrange
+
+            var cal = new BasicOperations(2, 5);
+
+            //Act
+
+            double result = cal.GangeFunktion();
+
+            //Assert
+
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod()]
+        public void DividerFunktionTest2()
+        {
+            //Arrange
+
+            var cal = new BasicOperations(4, 2);
+
+            //Act
+
+            double result = cal.DividerFunktion();
+
+            //Assert
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
