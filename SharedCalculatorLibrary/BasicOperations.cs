8e720a08066f32581da2446bba4046a9a5745875
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-04 13:38:24 +0200

    BasicCalculator operationer er defineret i void metoder i klassen BasicOperations.

---
+++ SharedCalculatorLibrary/BasicOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculatorLibrary
+{
+    public class BasicOperations
+    {
+        public double tal1 { get; set; }
+        public double tal2 { get; set; }
+
+        private void PlusFunktion()
+        {
+            (tal1 + tal2).ToString();           
+        }
+
+        private void MinusFunktion()
+        {         
+            (tal1 - tal2).ToString();
+        }
+
+        private void GangeFunktion()
+        {       
+            (tal1 * tal2).ToString();         
+        }
+
+        private void DividerFunktion()
+        {       
+           (tal1 / tal2).ToString();         
+        }
+    }
+}
